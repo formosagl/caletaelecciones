6c7ac2b097b0e4466e0d1de6931a128642a5e44a
José Manuel Gon <josemgon@hotmail.com>
2023-08-17 17:08:08 -0300

    fix: icons feature: register new user

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -39,13 +39,13 @@
                   </form>
                 </div>
                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                <!--small class="text-muted">Forgot you password? Reset you password 
-                  <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
-                </small>
+                <!--small class="text-muted">¿Olvidaste tu clave? Crea una 
+                  <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">nueva clave</a>
+                </small-->
                   <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
-                  </p-->
+                    ¿No tienes cuenta?
+                    <a href="register" class="text-info text-gradient font-weight-bold">Registrase</a>
+                  </p>
                 </div>
               </div>
             </div>
